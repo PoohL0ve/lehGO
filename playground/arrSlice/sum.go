05747ad32493e0835ed5ed4ae09f52c1f9ba6648
PoohL0ve <screwed06@gmail.com>
2026-09-11 23:45:03 -0400

    Arrays and Slices 5: Sum tail values and handled empty arrays

--- playground/arrSlice/sum.go
+++ playground/arrSlice/sum.go
@@ -18,3 +18,17 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return addAll
 }
+
+func SumAllTails(figuresToSum ...[]int) []int {
+	var tailsSum []int
+
+	for _, figures := range figuresToSum {
+		if len(figures) == 0 {
+			tailsSum = append(tailsSum, 0)
+		} else {
+			extract := figures[1:]
+			tailsSum = append(tailsSum, Sum(extract))
+		}
+	}
+	return tailsSum
+}
--- playground/arrSlice/sum_test.go
+++ playground/arrSlice/sum_test.go
@@ -25,3 +25,27 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("Wanted: %d but Got: %d", expected, actual)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	// Hides a variables and functions that do not need to be exposed
+	// ...also adds some type-safety
+	checksums := func(t *testing.T, actual, expected []int) {
+		t.Helper()
+		if !slices.Equal(actual, expected) {
+			t.Errorf("Wanted: %d but Got: %d", expected, actual)
+		}
+	}
+
+	t.Run("Sum of tails for slices with size more than on", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2, 4}, []int{7, 3})
+		expected := []int{6, 3}
+		checksums(t, actual, expected)
+	})
+
+	t.Run("Handling empty slices with 0", func(t *testing.T) {
+		actual := SumAllTails([]int{}, []int{3, 5, 7}, []int{9, 8})
+		expected := []int{0, 12, 8}
+		checksums(t, actual, expected)
+	})
+
+}
